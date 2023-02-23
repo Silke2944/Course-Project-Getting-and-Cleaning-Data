0f280877181e62610338ad355496a0a19e4b9d5a
Silke2944 <123584697+Silke2944@users.noreply.github.com>
2023-02-23 16:59:27 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,20 @@
 
         
 # Download data
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
 # Assign the files to variables
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
